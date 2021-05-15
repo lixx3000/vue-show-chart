341b6b0eeb6c8cbb58f3b84751b17306f04f16ea
lixx3000 <45725941+lixx3000@users.noreply.github.com>
2021-05-15 10:31:45 +0800

    Update no_data.js
    Version 1.0.1 fix：原来只能监听echart.init，现在可以监听setOption来判断是否有数据

--- src/directives/no_data.js
+++ src/directives/no_data.js
@@ -1,18 +1,29 @@
 import echarts from 'echarts';
 import echartUpdateDom from '@/directives/echart';
+import nextTick from '@/utils/nextTick';
 
 export default {
   install (Vue, opt) {
     const { text } = opt || {};
     // 监听数据指令，如果没有数据替换图表为其他页面
-    Vue.directive('show-chart', {
+    Vue.directive('show-echart', {
       bind (el) {
-        const echartInstance = echarts.getInstanceByDom(el);
-        if (echartInstance) {
-          const observer = new MutationObserver(() => echartUpdateDom(el, echartInstance, text));
+          const observer = new MutationObserver(() => {
+            const echartInstance = echarts.getInstanceByDom(el);
+            if (echartInstance) {
+              const setOption = echartInstance.setOption.bind(echartInstance);
+              const updateDom = () => echartUpdateDom(el, echartInstance, text);
+              Object.defineProperty(echartInstance, 'setOption', {
+                value (option, notMerge, lazyUpdate) {
+                  // 异步更新可以获取到最新的option
+                  nextTick(updateDom);
+                  setOption(option, notMerge, lazyUpdate);
+                }
+              });
+            }
+          });
           observer.observe(el, { attributes: true });
         }
-      }
     });
   }
 };
