18d67c8315761bd3c2c21dd2613052e0b59a5511
lixx3000 <45725941+lixx3000@users.noreply.github.com>
2021-05-17 10:23:40 +0800

    Update no_data.js
    解决暂无数据回显图表时，块大小不自适应问题

--- src/directives/no_data.js
+++ src/directives/no_data.js
@@ -1,6 +1,5 @@
 import echarts from 'echarts';
 import echartUpdateDom from './echart';
-import nextTick from '../utils/nextTick';
 
 export default {
   install (Vue, opt) {
@@ -12,13 +11,23 @@ export default {
             // const _echarts_instance_ = el.getAttribute('_echarts_instance_');
             if (echartInstance) {
               const setOption = echartInstance.setOption.bind(echartInstance);
+              let hasData = true;
               if (!echartInstance.setOption.didIntercept) {
-                const updateDom = (option) => () => echartUpdateDom(Vue, el, option, text);
+                const updateDomByOption = (option) => echartUpdateDom(Vue, el, echartInstance, option, text);
                 Object.defineProperty(echartInstance, 'setOption', {
                   value (option, notMerge, lazyUpdate) {
-                    // el.setAttribute('_echarts_instance_', _echarts_instance_);
-                    nextTick(updateDom(option));
-                    Promise.resolve().then(() => setOption(option, notMerge, lazyUpdate));
+                    hasData = updateDomByOption(option);
+                    setOption(option, notMerge, lazyUpdate);
+                  },
+                  configurable: true,
+                  writable: true
+                });
+                const resize = echartInstance.resize.bind(echartInstance);
+                Object.defineProperty(echartInstance, 'resize', {
+                  value (opt) {
+                    if (hasData) {
+                      resize(opt);
+                    }
                   },
                   configurable: true,
                   writable: true
