4f184958b9aefd751ad740e228adab6516679dff
lixx3000 <45725941+lixx3000@users.noreply.github.com>
2021-05-17 09:10:41 +0800

    Update no_data.js

--- src/directives/no_data.js
+++ src/directives/no_data.js
@@ -1,25 +1,30 @@
 import echarts from 'echarts';
-import echartUpdateDom from '@/directives/echart';
-import nextTick from '@/utils/nextTick';
+import echartUpdateDom from './echart';
+import nextTick from '../utils/nextTick';
 
 export default {
   install (Vue, opt) {
     const { text } = opt || {};
-    // 监听数据指令，如果没有数据替换图表为其他页面
     Vue.directive('show-echart', {
       bind (el) {
           const observer = new MutationObserver(() => {
             const echartInstance = echarts.getInstanceByDom(el);
+            // const _echarts_instance_ = el.getAttribute('_echarts_instance_');
             if (echartInstance) {
               const setOption = echartInstance.setOption.bind(echartInstance);
-              const updateDom = () => echartUpdateDom(el, echartInstance, text);
-              Object.defineProperty(echartInstance, 'setOption', {
-                value (option, notMerge, lazyUpdate) {
-                  // 异步更新可以获取到最新的option
-                  nextTick(updateDom);
-                  setOption(option, notMerge, lazyUpdate);
-                }
-              });
+              if (!echartInstance.setOption.didIntercept) {
+                const updateDom = (option) => () => echartUpdateDom(Vue, el, option, text);
+                Object.defineProperty(echartInstance, 'setOption', {
+                  value (option, notMerge, lazyUpdate) {
+                    // el.setAttribute('_echarts_instance_', _echarts_instance_);
+                    nextTick(updateDom(option));
+                    Promise.resolve().then(() => setOption(option, notMerge, lazyUpdate));
+                  },
+                  configurable: true,
+                  writable: true
+                });
+                echartInstance.setOption.didIntercept = true;
+              }
             }
           });
           observer.observe(el, { attributes: true });
